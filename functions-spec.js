676c0deb11e482cb6ce7ee3eb8b5bebb584ba4a4
Jeff Richardson <jeff@masterpieceinteractive.com>
2015-10-13 21:20:39 -0500

    working identity test

--- functions-spec.js
+++ functions-spec.js
@@ -3,7 +3,7 @@ let assert = require('assert');
 
 describe('the identity function', () => {
   it('should return the number passed in as the argument', () => {
-    const actual = f.identity(3);
+    const actual = f.identity(2);
     const expected = 2;
 
     assert.equal(actual, expected);
--- public/functions-spec.js
+++ public/functions-spec.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 
 describe('the identity function', function () {
   it('should return the number passed in as the argument', function () {
-    var actual = f.identity(3);
+    var actual = f.identity(2);
     var expected = 2;
 
     assert.equal(actual, expected);
